0bd9e30469f01d1500ac4061a3b8dfb3ac4f184f
Garg <2492357@cognizant.com>
2026-06-21 15:10:52 +0530

    Resolved conflict in app.routes.ts after stash pop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './Login/login/login';
 import { Register } from './Register/register/register';
+//import { Technician } from './Technicians/models/technician';
+import { ShowTechnician } from './Technicians/show-technician/show-technician';
+import { AddTechnician } from './Technicians/add-technician/add-technician';
+import { MainTech } from './Technicians/main-tech/main-tech';
 
 import { Navigation } from './Asset/navigation/navigation';
 import { AddAsset } from './Asset/add-asset/add-asset';
@@ -9,14 +13,18 @@ import { UpdateAsset } from './Asset/update-asset/update-asset';
 
 
 export const routes: Routes = [
+    
     {path:'login',component:Login},
     {path:'register',component:Register},
+    {path:'technician',component:ShowTechnician},
+     { path: 'add-tech', component: AddTechnician },
+    {path:'technician',component:MainTech}
     {path:'asset-navigation',component:Navigation,
         children:[
             {path:'add-asset', component:AddAsset},
             {path:'show-asset',component:ShowAssets},
-            {path:'update-asset/:assetID',component:UpdateAsset},
-
+            {path:'update-asset/:id',component:UpdateAsset}
         ]
     }
+    
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Login } from './Login/login/login';
 import { Navigation } from './navigation/navigation';
 import { Register } from './Register/register/register';
+import { ShowTechnician } from './Technicians/show-technician/show-technician';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navigation],
+ // imports: [RouterOutlet,Navigation],
+  imports: [RouterOutlet,ShowTechnician],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
